6a1a39135e061a710e7d8ca2142358038d76c4ff
Tang Zijian <tangzj77@gmail.com>
2014-10-16 12:12:02 +0800

    finish drag and drop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,10 +20,22 @@ window.Playground = {
                 $("#editor_workspace li").removeClass("draggable ui-draggable ui-draggable-handle");
                 $("#editor_workspace ul").addClass("ui-sortable");
                 $("#editor_workspace ul").sortable({
-                     connectWith: "#editor_workspace ul"
+                    connectWith: "#editor_workspace ul"
                 });
             }
         });
+        $("#editor_workspace ul").sortable({
+            stop: function(event, ui) {
+                var controlBlocks = $("#workspace-sortable li").has("li");
+                // controlBlocks.find(".block-control-botm").last().css("margin-top","-14px");
+                var subBlocks = controlBlocks.find("li");
+                var height = 97;
+                if (subBlocks.length>1) {
+                    height =  97 + (subBlocks.length-1)*33;
+                }
+            }
+        });
+        $("#editor_workspace ul").sortable("refreshPositions");
     }
 };
 
