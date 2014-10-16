8652d7b9f26c6425d6e793a8d9daa255a459046b
Zheng Pingxia <zhengpingxia@gmail.com>
2014-10-17 02:51:18 +0800

    implemented sprite.js, without repeat function

--- app/scripts/models/sprite.js
+++ app/scripts/models/sprite.js
@@ -8,6 +8,11 @@ Playground.Models = Playground.Models || {};
     Playground.Models.Sprite = Backbone.Model.extend({
 
         url: '',
+        costume: '',
+        xPos: 0,
+        yPos: 0,
+        isShown: true,
+        var array_of_functions = [],
 
         commands: {},
 
@@ -15,6 +20,10 @@ Playground.Models = Playground.Models || {};
         },
 
         defaults: {
+            xPos = 0;
+            yPos = 0;
+            isShown = true;
+            costume = 'defaultLink';
         },
 
         validate: function(attrs, options) {
@@ -23,6 +32,44 @@ Playground.Models = Playground.Models || {};
         parse: function(response, options)  {
             return response;
         }
+
+        add: function(index, position, parameters){
+           switch(index){
+                case 0:
+                array_of_functions.splice(position,0,setXPos(parameters[0]));
+                break;
+                case 1:
+                array_of_functions.splice(position,0,setYPos(parameters[0]));
+                break;
+                case 2:
+                array_of_functions.splice(position, 0, changeCostume(parameters[0]));
+                break;
+           }
+        }
+
+        deleteCommand: function(position){
+
+        }
+
+        setXPos: function(xPosition){
+            xPos = xPosition;
+             var obj = {xPos: xPos, yPos: yPos, isShown: isShown, costume: costume};
+            return obj;
+        }
+
+        setYPos: function(yPosition){
+            yPos = yPosition;
+            var obj = {xPos: xPos, yPos: yPos, isShown: isShown, costume: costume};
+            return obj;
+        }
+
+        changeCostume: function(link){
+            costume = link;
+            var obj = {xPos: xPos, yPos: yPos, isShown: isShown, costume: costume};
+            return obj;
+        }
+
+
     });
 
 })();
